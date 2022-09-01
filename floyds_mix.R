a8de8e27789e9d31a9400cefdf1437784ab70a84
Elle Pattenden <48939952+EllePattenden@users.noreply.github.com>
2022-09-01 16:52:55 +1000

    generate p space and chunk

---
+++ floyds_mix.R
@@ -0,0 +1,111 @@
+# OzStar part 1 (pull apart Min et al.)
+# generate parameter space for sims 
+
+##-------------------------------------------------------------------------#
+
+library(data.table)
+library(tidyverse)
+library(igraph)
+library(Matrix)
+
+
+rm(list = ls())      # clear environment 
+options(scipen=999)  # stop printing in scientific notation, please !
+set.seed(31133113)      # swap so each make brain do sense thing and batch 
+igraph_options(sparsematrices = TRUE)  # work with sparse matrices, please
+
+# -------------------------- Parameters ----------------------------------#
+
+# Fixed 
+params <- setDT(data.frame(
+  N = 100,   # number of agents 
+  # Resource related
+  initi_R = 50,   # initial size of resource
+  max_R = 200,    # resource carrying capacity
+  d = 50,         # natural discharge rate
+  # Resource 
+  E_opt = 0.483,  # optimal effort (water)
+  q = 1,          # technology quotient 
+  w = 15,         # opportunity cost per unit effort 
+  # Production (Cobb-Douglas)
+  alpha = 0.6,    
+  beta = 0.2,     # with a + b < 1 
+  gamma = 10,     # for diminishing returns to scale 
+  # Ostracism (Gompertz function)
+  h = .17,        # maximum sanctioning (asymptote)      
+  # Noting that all papers bar Min et al. use ~.333 !!! 
+  t = -150,       # sanctioning effectiveness threshold
+  g = -10,        # sanctioning growth rate
+  # Other 
+  max_t = 10^5,    # max time steps in each  simulation i.e., time for
+  # the system to equilibrate; 50,000 in TLS et al. (2016)
+  # 100,000 in Min et al. (2018) because ties continue to be
+  # updated until homogeneous communities form 
+  # will break this is subsequent sims by using utility comps.
+  p_coop = 0.99,   # initial proportion of cooperators
+  mu = 3.9,        # "cheating multiplier"; 0 < mu < 3.9, where 3.9 = Nash
+  # (can be overwritten for a more precise value below) 
+  negative_utilities = TRUE,   # can agent utilities be negative? logical;
+  # false = they can't. TLS et al. (2016) said no 
+  # (Min et al. allowed, I think)    
+  # ******** now overwritten to false in one_run_min.R 
+  update_prob = 0.01,  # proportion of agents comparing utilities in each tick 
+  # referred to as "strategy updating probability" in some
+  network_type = "ER_random",   # the initial network structure
+  scheduling_dynamics = "seperate",  # how tie formation plays out. 
+  # For 'seperate' (here), the probability of rewiring 
+  # probability is determined by rho while the probability 
+  # for strategy updating is proportional to the payoff 
+  # difference. 
+  tie_strat = "min_method"  # how agent's rewire their networks
+  # as this simulation seperates the prob. of strategy 
+  # updating from the prob of network rewiring. Two agents 
+  # are randomly selected each tick; one for each social 
+  # process.
+  # noting that the last 5 parameters are set here for reference only. 
+))
+params[, "e_coop" := E_opt / N ]    # effort for cooperators 
+params[, "e_defect" := e_coop *  mu]    # defector's effort
+# params[, "e_defect" := 1.826 / N]   # ' ' if you want the rounding to be exact 
+
+# compromise for Floyd = 64260 runs 
+#replicate <- seq(1, 30, 1)         # 30 reps of each
+#degree <- c(15, 90)                # comparing average degree of 15 and 90
+#resource_inflow <- seq(10, 60, 1)  # across c [10:60]
+#rho <- sort(seq(0, 1, 0.05), decreasing = TRUE) # with rho [0:1]
+
+# compromised again because I (Elle) want to get on with more interesting stuff 
+# = 42840 runs 
+replicate <- seq(1, 20, 1)         # 20 reps of each
+degree <- c(15, 90)                # comparing average degree of 15 and 90
+resource_inflow <- seq(10, 60, 1)  # across c [10:60]
+rho <- sort(seq(0, 1, 0.05), decreasing = TRUE) # with rho [0:1]
+
+
+# stick all parameters in one big data.table and save as test_combo.Rda
+combos <- setDT(expand.grid(replicate, degree, resource_inflow, rho))
+setnames(combos, new = c("replicate", "degree", "resource_inflow", "rho"))
+combo <- cbind(combos, params)   # each row has parameter values for a run 
+save(combo, file="test_combo.Rda")
+
+# create chunked version and save as test_combo_chunked.Rda
+length <- ceiling(nrow(combo)/2000)   # chunks required
+out <- vector("list", length = length)   
+for (i in 1:length(out)) { 
+start <- (i*2000) - 1999
+print(start)
+row <- i*2000
+print(row)
+out[[i]] <- combo[start:row, ]
+}
+# drop NA rows from chunk 22 
+out[[22]] <- na.omit(out[[22]])
+combo <- out
+save(combo, file = "test_combo_chunked.Rda")
+
+# keep it clean(-ish) 
+rm(list = c("replicate", "degree", "resource_inflow", "rho", "combos", "params"))
+
+
+
+
