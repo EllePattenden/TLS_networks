65906f29084009742b54dc23b9aa5cc62493e820
Elle Pattenden <48939952+EllePattenden@users.noreply.github.com>
2022-09-03 08:54:16 +1000

    ¯\_(ツ)_/¯

--- SLURM/Spartan/floyd_part2_chunked.sh
+++ SLURM/Spartan/floyd_part2_chunked.sh
@@ -3,18 +3,18 @@
 #SBATCH --job-name=refute_min_chunk
 #SBATCH --output=/data/gpfs/projects/punim1783/log/floyds_mix/%j
 #SBATCH --ntasks=1 
-#SBATCH --array=1-2000          
-#SBATCH --mem-per-cpu=4000            # TEST MEMORY REQS NOW THAT WE AREN'T SAVING ALL THE BLOODY NETWORKS
+#SBATCH --array=1-2000           
+#SBATCH --mem-per-cpu=2500            # TEST MEMORY REQS NOW THAT WE AREN'T SAVING ALL THE BLOODY NETWORKS
 
 # chunked version of code to get around max_array 
 # cd /data/gpfs/projects/punim1783/jobs    
-# chunk=1     # a number 1-22 ** number 22 only has 840 rows, so ammend when get around to that! 
+# chunk=21     # a number 1-22 ** number 22 only has 840 rows, so ammend when get around to that! 
+               # working backwards from 22 now 
 ## then sbatch --job-name=refute_min_$chunk --export=chunk=$chunk floyd_part2_chunked.sh
 
-
 SCRATCH_DIRECTORY=/data/gpfs/projects/punim1783/working/${SLURM_JOBID}
-CODE_DIRECTORY=/data/gpfs/projects/punim1783/code
-OUTPUT_DIRECTORY=/data/gpfs/projects/punim1783/output/floyds_mix/sim_results
+CODE_DIRECTORY=/data/gpfs/projects/punim1783/code/
+OUTPUT_DIRECTORY=/data/gpfs/projects/punim1783/output/floyds_mix/sim_results/
 mkdir -p ${SCRATCH_DIRECTORY}
 mkdir -p ${OUTPUT_DIRECTORY}
 cp ${CODE_DIRECTORY}/*.R ${SCRATCH_DIRECTORY}
@@ -29,3 +29,4 @@ cp ${SCRATCH_DIRECTORY}/replicating_min*.Rdata  ${OUTPUT_DIRECTORY}
 T="$(($(date +%s)-T))"
 echo "run # ${SLURM_ARRAY_TASK_ID} took ${T} seconds"
 rm -rf ${SCRATCH_DIRECTORY}
+
