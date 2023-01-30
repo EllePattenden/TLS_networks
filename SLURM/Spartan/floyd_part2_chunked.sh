b602312256bdd8f685657ef89ad12de3c686f234
Elle Pattenden <48939952+EllePattenden@users.noreply.github.com>
2023-01-31 09:06:51 +1100

    Update floyd_part2_chunked.sh
    updated to grab desired model using $version + $go vars to avoid overwriting model scripts and make less of a mess outputting

--- SLURM/Spartan/floyd_part2_chunked.sh
+++ SLURM/Spartan/floyd_part2_chunked.sh
@@ -1,32 +1,45 @@
 #!/bin/bash
-#SBATCH --time=14:00:00
+#SBATCH --time=7:30:00
 #SBATCH --job-name=refute_min_chunk
-#SBATCH --output=/data/gpfs/projects/punim1783/log/floyds_mix/%j
+#SBATCH --output=/data/gpfs/projects/punim1783/log/floyds_mix/chunk20/%j  
 #SBATCH --ntasks=1 
-#SBATCH --array=1-2000           
-#SBATCH --mem-per-cpu=2500            # TEST MEMORY REQS NOW THAT WE AREN'T SAVING ALL THE BLOODY NETWORKS
+#SBATCH --array=1-2000                          
+#SBATCH --mem-per-cpu=3500        # 2500 more than enough for low rho      
 
-# chunked version of code to get around max_array 
 # cd /data/gpfs/projects/punim1783/jobs    
-# chunk=21     # a number 1-22 ** number 22 only has 840 rows, so ammend when get around to that! 
-               # working backwards from 22 now 
-## then sbatch --job-name=refute_min_$chunk --export=chunk=$chunk floyd_part2_chunked.sh
+# chunk=XX                                  # a number 1-22 (noting chunk 22 only has 840 rows)
+# version="actually_rep"      | version="rho_sep"  
+# go="original"               | go="r50"
+# echo "doing chunk: " $chunk " version: " $version "go procedure: " $go
+# sbatch --job-name=refute_min_$chunk --export=chunk=$chunk,version=$version,go=$go floyd_part2_chunked.sh
 
 SCRATCH_DIRECTORY=/data/gpfs/projects/punim1783/working/${SLURM_JOBID}
 CODE_DIRECTORY=/data/gpfs/projects/punim1783/code/
-OUTPUT_DIRECTORY=/data/gpfs/projects/punim1783/output/floyds_mix/sim_results/
-mkdir -p ${SCRATCH_DIRECTORY}
-mkdir -p ${OUTPUT_DIRECTORY}
+if [[ ${version} == "actually_rep" ]]; then 
+    if [[ ${go} == "original" ]]; then 
+        OUTPUT_DIRECTORY=/data/gpfs/projects/punim1783/output/floyds_mix/sim_results/toreproducemin/
+    else 
+        OUTPUT_DIRECTORY=/data/gpfs/projects/punim1783/output/floyds_mix/sim_results/toreproducemin/R50/
+    fi 
+else 
+    if [[ ${go} == "original" ]]; then
+        OUTPUT_DIRECTORY=/data/gpfs/projects/punim1783/output/floyds_mix/sim_results/
+    else 
+        OUTPUT_DIRECTORY=/data/gpfs/projects/punim1783/output/floyds_mix/sim_results/R50/
+    fi
+fi
+mkdir -p -v ${OUTPUT_DIRECTORY}
+mkdir -p -v /data/gpfs/projects/punim1783/log/floyds_mix/chunk${chunk}/
+mkdir -p -v ${SCRATCH_DIRECTORY}
 cp ${CODE_DIRECTORY}/*.R ${SCRATCH_DIRECTORY}
 cp /data/gpfs/projects/punim1783/output/floyds_mix/test_combo_chunked.Rda ${SCRATCH_DIRECTORY}
 cd ${SCRATCH_DIRECTORY}
 module load r/4.0.0 
 T="$(date +%s)"
 echo "beginning chunk ${chunk} run ${SLURM_ARRAY_TASK_ID}"
-Rscript part2_chunked.R
-ls ${SCRATCH_DIRECTORY}/replicating_min*.Rdata
-cp ${SCRATCH_DIRECTORY}/replicating_min*.Rdata  ${OUTPUT_DIRECTORY}
+Rscript part2_chunked.R  ${chunk} ${version} ${go}          # this script will use version and go to call the desired GO script 
+ls ${SCRATCH_DIRECTORY}/*replicating_min*.Rdata
+cp ${SCRATCH_DIRECTORY}/*replicating_min*.Rdata  ${OUTPUT_DIRECTORY}
 T="$(($(date +%s)-T))"
 echo "run # ${SLURM_ARRAY_TASK_ID} took ${T} seconds"
 rm -rf ${SCRATCH_DIRECTORY}
-
