d44b1be1ccfbec932570213d78e3030215391c92
Elle Pattenden <48939952+EllePattenden@users.noreply.github.com>
2023-01-31 18:05:18 +1100

    Update smoosh.R
    generalised using $version and $go

--- code/R/smoosh.R
+++ code/R/smoosh.R
@@ -1,11 +1,29 @@
 library(data.table)
 
+args <- commandArgs(trailingOnly=TRUE) 
+print(args) 
+version <- args[1]
+go <- args[2]
+
 files <- list.files(
-  "/data/gpfs/projects/punim1783/output/floyds_mix/cleaned_sim_results/to_smoosh",
-  recursive = FALSE,     # don't go looking in results sub 
-  pattern = ".RData", 
-  full.names = TRUE
-  )
+    if(version == "actually_rep") {
+      if (go == "original") {
+         "/data/gpfs/projects/punim1783/output/floyds_mix/cleaned_sim_results/actually_replicating_min/to_smoosh"
+      } else {
+        "/data/gpfs/projects/punim1783/output/floyds_mix/cleaned_sim_results/actually_replicating_min/to_smoosh_R50/"
+      }   
+    } else {
+        if (go == "original") {
+          "/data/gpfs/projects/punim1783/output/floyds_mix/cleaned_sim_results/to_smoosh2"    # to_smoosh2 for rho_sep 
+      } else {
+        "/data/gpfs/projects/punim1783/output/floyds_mix/cleaned_sim_results/to_smoosh_R50/"
+      }   
+    }, 
+    recursive = FALSE,      # no sub folders 
+    pattern = "^smoosh",   
+    full.names = TRUE       # need the file path to load
+)
+#print(files)
 
 summary <- data.table(rep = NA,               # output for ID + plotting 
                       ri = NA,                # resource inflow
@@ -19,34 +37,50 @@ summary <- data.table(rep = NA,               # output for ID + plotting
                       num_strat_swaps = NA,    # tally times strategies swapped,
                       final_CC = NA,          # proportion C-C ties at t_fin
                       final_CD = NA,          # proportion C-D ties at t_fin
-                      final_DD = NA           # proportion D-D ties at t_fin
+                      final_DD = NA,           # proportion D-D ties at t_fin
+                      num_strat_path_entered = NA
 )
 
 for (f in files) { 
   load(f)    # loads as 'out'
+  print(f)
   print(out)
-  summary <- rbind(summary, out)
+  summary <- rbind(summary, out, fill=TRUE)
   rm(out)
-  }
-
-save(summary, file = "summary_min.RData")
-
-# make heatmap
-library(tidyverse)
-summary <- as.data.frame(summary) 
-fig3_15 <- summary %>% 
-  filter(degree = 15) %>% 
-  ggplot(aes(x = ri, y = rho, fill = prop_coop)) +
-  theme_classic() + 
-  xlab("resource inflow, c") + 
-  ylab("rewiring probability")
-ggsave("figure_three_15.png")
-fig3_90 <- summary %>% 
-  filter(degree = 90) %>% 
-  ggplot(aes(x = ri, y = rho, fill = prop_coop)) +
-  theme_classic() + 
-  xlab("resource inflow, c") + 
-  ylab("rewiring probability") 
-ggsave("figure_three_90.png")
+}
+rm(f)
 
 
+if(version == "actually_rep") {
+    if (go == "original") {
+        save(summary, file = "summary_min.RData")
+    } else {
+        save(summary, file = "summary_min_R50.RData")
+    }   
+} else {
+    if (go == "original") {
+        save(summary, file = "RHO_summary_min.RData") 
+    } else {
+        save(summary, file = "RHO_summary_min_R50.RData") 
+    }
+}   
+    
+# make v rough heatmaps
+# library(tidyverse)
+# summary <- as.data.frame(summary) 
+# fig3_15 <- summary %>% 
+#   filter(degree == 15) %>% 
+#   ggplot(aes(x = ri, y = rho, fill = f_prop_coop)) +
+#   geom_tile() + 
+#   theme_classic() + 
+#   xlab("resource inflow, c") + 
+#   ylab("rewiring probability")
+# ggsave("figure_three_15.png")
+# fig3_90 <- summary %>% 
+#   filter(degree == 90) %>% 
+#   ggplot(aes(x = ri, y = rho, fill = f_prop_coop)) +
+#   geom_tile() + 
+#   theme_classic() + 
+#   xlab("resource inflow, c") + 
+#   ylab("rewiring probability") 
+# ggsave("figure_three_90.png")
